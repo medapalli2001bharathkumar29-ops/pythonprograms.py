dd109013ff3933d0e52786438923c58db7d0ba07
medapalli2001bharathkumar29-ops <medapalli2001bharathkumar29@gmail.com>
2026-06-04 21:06:15 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,91 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #variables
+>>> print(7+9)
+16
+>>> a=10
+>>> print(a)
+10
+>>> b=30
+>>> print(b)
+30
+>>> x=100
+>>> print(x)
+100
+>>> print(X)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+>>> print(x)
+100
+z=50
+print(z)
+50
+5=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+7=100
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a7=100
+print(a7)
+100
+20a=40
+SyntaxError: invalid decimal literal
+a0123456789=30
+print(ao123456789)
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    print(ao123456789)
+NameError: name 'ao123456789' is not defined. Did you mean: 'a0123456789'?
+print(a0123456789
+print(0123456789)
+      
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+a0123456789=30
+      
+print(a0123456789)
+      
+30
+0=10
+      
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+name="bharath"
+      
+print(name)
+      
+bharath
+place="vij"
+      
+print(place)
+      
+vij
+country="india"
+      
+print=country
+      
+print=(country)
+      
+country="india"
+      
+print(country)
+      
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    print(country)
+TypeError: 'str' object is not callable
+country="india"
+      
+print(country)
+      
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(country)
+TypeError: 'str' object is not callable
+country="india"
+      
+print(country)
+      
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    print(country)
+TypeError: 'str' object is not callable
