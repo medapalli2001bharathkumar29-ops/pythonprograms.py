5909b550a77d03edfd6c30c4951269c444537950
medapalli2001bharathkumar29-ops <medapalli2001bharathkumar29@gmail.com>
2026-06-05 17:11:08 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=7
+>>> type(a)
+<class 'int'>
+>>> a=6.7
+>>> type(b)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    type(b)
+NameError: name 'b' is not defined
+>>> b=6.7
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="course"
+>>> type(d)
+<class 'str'>
+>>> e='''python'''
+>>> type(e)
+<class 'str'>
